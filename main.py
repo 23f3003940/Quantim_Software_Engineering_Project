799191a12a9cfabde7e09b1b79e99362a5dd1f03
rishikeshgupta12 <rishikeshmmss@gmail.com>
2026-06-02 00:10:49 +0530

    Task 2: Data Processing

--- main.py
+++ main.py
@@ -1,16 +1,24 @@
-# This is a sample Python script.
+import csv
 
-# Press Ctrl+F5 to execute it or replace it with your code.
-# Press Double Shift to search everywhere for classes, files, tool windows, actions, and settings.
+lst = []
+def readcsv(filename):
+    with open(filename, newline='\n') as csvfile:
+        reader = csv.reader(csvfile, delimiter=',')
+        for row in reader:
+            if row[0] == 'pink morsel':
+                sales = float(row[1][1:]) * int(row[2])
+                l = []
+                l.append('$' + str(sales))
+                l.append(row[3])
+                l.append(row[4])
+                lst.append(l)
 
+readcsv('data/daily_sales_data_0.csv')
+readcsv('data/daily_sales_data_1.csv')
+readcsv('data/daily_sales_data_2.csv')
 
-def print_hi(name):
-    # Use a breakpoint in the code line below to debug your script.
-    print(f'Hi, {name}')  # Press F9 to toggle the breakpoint.
-
-
-# Press the green button in the gutter to run the script.
-if __name__ == '__main__':
-    print_hi('PyCharm')
-
-# See PyCharm help at https://www.jetbrains.com/help/pycharm/
+with open('data/output.csv', 'w', newline='\n') as csvfile:
+    writer = csv.writer(csvfile, delimiter=',')
+    writer.writerow(['Sales', 'Date', 'Region'])
+    for row in lst:
+        writer.writerow(row)
